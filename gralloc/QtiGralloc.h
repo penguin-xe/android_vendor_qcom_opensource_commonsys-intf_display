4ae1eda91458781e46d09b4c0cd7f45bceb31588
Neha Narayana Swamy Gowda <quic_nehanara@quicinc.com>
2025-03-31 14:05:35 -0700

    display: added metadata for multi view Id
    Change-Id: Ie4907658b530d40cb95150e220b97e34c7e6351d

--- gralloc/QtiGralloc.h
+++ gralloc/QtiGralloc.h
@@ -28,8 +28,8 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -150,6 +150,7 @@ static const MetadataType MetadataType_BaseView = {VENDOR_QTI, QTI_BASE_VIEW};
 static const MetadataType MetadataType_MultiViewInfo = {VENDOR_QTI, QTI_MULTI_VIEW_INFO};
 static const MetadataType MetadataType_ThreeDimensionalRefInfo = {
     VENDOR_QTI, QTI_THREE_DIMENSIONAL_REF_INFO};
+static const MetadataType MetadataType_ViewId = {VENDOR_QTI, QTI_VIEW_ID};
 
 // 0 is also used as invalid value in standard metadata
 static const MetadataType MetadataType_Invalid = {VENDOR_QTI, 0};
--- gralloc/QtiGrallocMetadata.h
+++ gralloc/QtiGrallocMetadata.h
@@ -28,8 +28,8 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -85,6 +85,7 @@
 #define QTI_BASE_VIEW 10047
 #define QTI_MULTI_VIEW_INFO 10048
 #define QTI_THREE_DIMENSIONAL_REF_INFO 10049
+#define QTI_VIEW_ID 10051
 // Used to indicate to framework that internal definitions are used instead
 #define COMPRESSION_QTI_UBWC 20001
 #define INTERLACED_QTI 20002
